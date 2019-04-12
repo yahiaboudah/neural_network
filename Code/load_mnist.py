6c59acbb871603b0d3d05a0371f749816298dd6b
Foxydoxy <34740761+Foxydoxy@users.noreply.github.com>
2019-04-12 22:55:49 +0100

    some changes

--- Code/load_mnist.py
+++ Code/load_mnist.py
@@ -1,17 +1,35 @@
 from mnist import MNIST
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+import timeit
+
+def load_data():
+    mndata = MNIST('Data')
+    loaded_training_data = mndata.load_training()
+    loaded_testing_data = mndata.load_testing()
+    return loaded_training_data,loaded_testing_data
 
 def get_data():
-    mndata = MNIST('Data')
-    training_data = mndata.load_training()
-    testing_data = mndata.load_testing()
+    loaded_training_data,loaded_testing_data = load_data()
+    training_inputs = [np.divide(np.reshape(x, (784, 1)),255) for x in loaded_training_data[0]]
+    training_labels = [vectorize(b) for b in loaded_training_data[1]]
+    training_data = list(zip(training_inputs,training_labels))
+    testing_inputs = [np.divide(np.reshape(x,(784,1)),255) for x in loaded_testing_data[0]]
+    testing_data = list(zip(testing_inputs,loaded_testing_data[1]))
+
     return training_data,testing_data
 
-#import matplotlib.pyplot as plt
+def vectorize(b):
+    m = np.zeros((10,1))
+    m[b] = 1
+    return m
+
 """
-index = random.randrange(0,len(testing_data))
-arr = (np.mat(np.array(training_data[index]))).reshape(28,28)
+print(timeit.timeit('get_data()',number=1,setup= 'from __main__ import get_data'))
+training_data,testing_data = get_data()
+index = random.randrange(0,len(training_data))
+arr = (np.mat(training_data[index][0])).reshape(28,28)
 plt.imshow(arr,cmap='binary')
 plt.show()
 """
--- Code/network.py
+++ Code/network.py
@@ -1,70 +1,86 @@
 import numpy as np
+import random as random
+import load_mnist as mn
 
 class Network(object):
 
-    def __init__(self,size,training_data,testing_data):
+    def __init__(self,size,training_data,testing_data,mini_batch_size,num_epochs,learning_rate):
         self.num_layers = len(size)
-        self.training_data = training_data
-        self.testing_data = testing_data
-        self.weights = [np.random.random((x,y)) for y,x in zip(size[:-1],size[1:])]
-        self.biases = [np.random.random((x,1)) for x in size[1:]]
+        self.learning_rate = learning_rate
+        self.size = size
+        self.mini_batch_size = mini_batch_size
+        self.weights = [np.random.random((x,y)) for y,x in zip(self.size[:-1],self.size[1:])]
+        self.biases = [np.random.random((x,1)) for x in self.size[1:]]
+        self.num_epochs = num_epochs
 
     def feed_forward(self,a,all_layers = True):
         if(all_layers):
             a = [a]
             for i in range(1,self.num_layers):
-                a[i] = sigmoid(np.dot(self.weights[i-1]+self.biases[i-1],a[i-1]))
+                a.append(self.sigmoid(np.dot(self.weights[i-1],a[i-1])+self.biases[i-1]))
         else:
-            for i in range(self.num_layers-1):
-                a = sigmoid(np.dot(self.weights[i]+self.biases[i],a))
+            for b,w in zip(self.biases,self.weights):
+                a = self.sigmoid(np.dot(w,a)+b)
         return a
 
     def backprop(self,a,y,dw,db):
         #Caclulate the delta of the last layer:
-        delta = (a[-1]-y) * sigmoid(a[-1],deriv=True)
+        delta = (a[-1]-y)
         #Update params:
-        dw[-1] += (np.dot(delta,a[-2].T))/mini_batch_size
-        db[-1] += (delta)/mini_batch_size
+        dw[-1] += ((np.dot(delta,a[-2].T))/self.mini_batch_size)
+        db[-1] += ((delta)/self.mini_batch_size)
 
-        for l in range(2,self.num_layers+1):
+        for l in range(2,self.num_layers):#num_layers+1 previously
             #Get delta of secondtolast layer:
-            delta = np.dot(self.weights[-l+1].T, delta) * sigmoid(a[-l],deriv=True)
-            db[-l] += delta/mini_batch_size
-            dw[-l] += (np.dot(delta,a[-l-1].T))/mini_batch_size
+            delta = np.dot(self.weights[-l+1].T, delta) * self.sigmoid_deriv(a[-l])
+            db[-l] += (delta/self.mini_batch_size)
+            dw[-l] += ((np.dot(delta,a[-l-1].T))/self.mini_batch_size)
         #Return the matrices
         return dw,db
 
 
     def update_given_mini_batch(self,mini_batch):
         #Create two empty matrices to store derivatives:
-        dw = [np.zeros((x,y)) for y,x in zip(self.size[:-1],self.size[1:])]
-        db = [np.zeros((x,1)) for x in self.size[1:]]
+        dw = [np.zeros(w.shape) for w in self.weights]
+        db = [np.zeros(b.shape) for b in self.biases]
         #Go through the training data to get derivatives:
         for x,y in mini_batch:
             a = self.feed_forward(x)
             dw,db = self.backprop(a,y,dw,db)
         #Now update the weights and biases given dw,db:
-        for i in range(num_layers-1):
-            self.weights[i] -= learning_rate*dw[i]
-            self.biases[i] -= learning_rate*db[i]
+        for i in range(self.num_layers-1):
+            self.weights[i] -= self.learning_rate*dw[i]
+            self.biases[i] -= self.learning_rate*db[i]
 
-    def stochastic_gradient_descent(self,num_epochs):
-        for i in range(num_epochs):
+    def stochastic_gradient_descent(self):
+        n = len(training_data)
+        random.shuffle(training_data)
+        for i in range(self.num_epochs):
             #Create a list of mini_batches here
-            random.shuffle(training_data)
-            mini_batches = [training_data[k:k+mini_batch_size]
-                            for k in range(0,len(training_data),mini_batch_size)]
+            mini_batches = [training_data[k:k+self.mini_batch_size] for k in range(0,n,self.mini_batch_size)]
             for mini_batch in mini_batches: #Loop through the data
-                update_given_mini_batch(mini_batch)
-
+                self.update_given_mini_batch(mini_batch)
+            print('Epoch ',(i+1),': training complete.')
             print('Testing Accuracy:', self.get_training_accuracy(),'/10000')
 
-    def get_training_accuracy():
-        for x in training_data:
-            a = feed_forward(x)
+    def get_training_accuracy(self):
+        i = 0
+        for x,y in training_data:
+            i+=1
+            if(i % 1000):
+                a = self.feed_forward(x,all_layers=False)
+                print(a)
+        res = [(np.argmax(self.feed_forward(x,all_layers=False)),np.argmax(y)) for x,y in training_data]
+        return sum(int(x == y) for (x,y) in res)
+
+    def sigmoid(self,x):
+        return 1/(1+np.exp(-x))
+
+    def sigmoid_deriv(self,x):
+        return x * (1-x)
 
 
+training_data,testing_data = mn.get_data()
 
-
-
-q
+net = Network([784,30,10],training_data,testing_data,20,5,1.5)
+net.stochastic_gradient_descent()
